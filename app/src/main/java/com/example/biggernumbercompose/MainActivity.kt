7a6bf3ce2af72b6070d8e1c69f9c904469543dd8
Jack <jack@example.com>
2022-03-18 10:53:48 +0700

    Use ViewModel

--- app/src/main/java/com/example/biggernumbercompose/MainActivity.kt
+++ app/src/main/java/com/example/biggernumbercompose/MainActivity.kt
@@ -8,12 +8,17 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import java.util.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,34 +29,54 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-fun randomPairOfNumbers(): Pair<Int, Int> {
-    val r = Random()
-    val first = r.nextInt(10)
-    var second = first
-    while (second == first) {
-        second = r.nextInt(10)
+class BiggerNumberViewModel: ViewModel() {
+    private var _leftNum = 0
+    private var _rightNum = 0
+    private val _points = MutableLiveData(0)
+
+    init {
+        assignRandomNumbers()
     }
-    return Pair(first, second)
+
+    val leftNum: Int
+        get() = _leftNum
+
+    val rightNum: Int
+        get() = _rightNum
+
+    val points: LiveData<Int>
+        get() = _points
+
+    fun assignRandomNumbers() {
+        _leftNum = Random.nextInt(10)
+        _rightNum = _leftNum
+        while (_rightNum == _leftNum)
+            _rightNum = Random.nextInt(10)
+    }
+
+    fun checkAnswer(leftNumSelected: Boolean): Boolean {
+        val isAnswerCorrect = if (leftNumSelected) _leftNum > _rightNum else _rightNum > _leftNum
+        _points.value?.let {
+            _points.value = if (isAnswerCorrect) it.plus(1) else it.minus(1)
+        }
+        return isAnswerCorrect
+    }
 }
 
 @Composable
 fun BiggerNumber() {
+    val viewModel: BiggerNumberViewModel = viewModel()
+    val points = viewModel.points.observeAsState()
     var isCorrect by remember { mutableStateOf(true) }
-    var points by remember { mutableStateOf(0) }
-    var (leftNum, rightNum) = randomPairOfNumbers()
+
     val checkAnswer = { isLeftButtonSelected: Boolean ->
-        isCorrect = if (isLeftButtonSelected) leftNum > rightNum else rightNum > leftNum
-        if (isCorrect) {
-            points++
-        } else {
-            points--
-        }
-        val pair = randomPairOfNumbers()
-        leftNum = pair.first
-        rightNum = pair.second
+        isCorrect = viewModel.checkAnswer(isLeftButtonSelected)
+        viewModel.assignRandomNumbers()
     }
     Column(
-        modifier = Modifier.fillMaxHeight().background(color = if (isCorrect) Color.Green else Color.Red ),
+        modifier = Modifier
+            .fillMaxHeight()
+            .background(color = if (isCorrect) Color.Green else Color.Red),
         verticalArrangement = Arrangement.SpaceBetween
     ) {
         Text(
@@ -68,11 +93,12 @@ fun BiggerNumber() {
                 .padding(horizontal = 8.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            NumberButton(number = leftNum, onClick = { checkAnswer(true) })
-            NumberButton(number = rightNum, onClick = { checkAnswer(false) })
+            NumberButton(number = viewModel.leftNum, onClick = { checkAnswer(true) })
+            NumberButton(number = viewModel.rightNum, onClick = { checkAnswer(false) })
         }
+
         Text(
-            text = "Points: $points",
+            text = "Points: ${points.value}",
             fontSize = 24.sp,
             textAlign = TextAlign.Center,
             modifier = Modifier
